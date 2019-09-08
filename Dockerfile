62a85b14d8eb71034fa1d43dc044e1cef62a1e1f
Daniel Tschan <tschan@puzzle.ch>
2019-09-08 19:33:32 +0200

    none

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+FROM websphere-liberty:19.0.0.3-webProfile8
+
+USER root
+COPY docker-prestart.sh /usr/local/bin/
+
+USER 1001
+
+ENTRYPOINT ["/usr/local/bin/docker-prestart.sh", "/opt/ibm/helpers/runtime/docker-server.sh", "/opt/ibm/wlp/bin/server", "run", "defaultServer"]
---
+++ docker-prestart.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ -e /etc/tls/private/tls.crt ] && [ -e /etc/tls/private/tls.key ]; then
+  KEYSTORE_FILE=/config/resources/security/key.p12
+  KEYSTORE_PASSWORD="$(dd if=/dev/urandom bs=43 count=1 status=none | base64 -w0)"
+
+  echo "Creating keystore from secret's cert data"
+  mkdir -p "$(dirname ${KEYSTORE_FILE})"
+  openssl pkcs12 -export -in /etc/tls/private/tls.crt -inkey /etc/tls/private/tls.key -out ${KEYSTORE_FILE} -name localhost -password pass:${KEYSTORE_PASSWORD}
+  if [ $? != 0 ]; then
+    echo "Failed to create a PKCS12 certificate file with the service-specific certificate. Aborting."
+    exit 1;
+  fi
+
+  echo "KEYSTORE_PASSWORD=${KEYSTORE_PASSWORD}" >>/config/bootstrap.properties
+fi
+
+exec "$@"
